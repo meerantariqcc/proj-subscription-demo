d174573a01d97976ace6331fe5d3be0db5141400
MeeranTariq <meerantariq5432@gmail.com>
2025-11-07 01:42:14 +0500

    Initial Commit

---
+++ SubscriptionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubscriptionDemo
+//
+//  Created by Meeran Tariq on 07/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
